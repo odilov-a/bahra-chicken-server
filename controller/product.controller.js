7cd45b502381a445d9f55b7d0bc4af94e6d8b82d
odilov-a <akbarjonodilov01@gmail.com>
2024-05-24 16:00:11 +0500

    chore: Update .env file with production URL for image uploads

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -24,53 +24,19 @@ exports.getProductById = async (req, res) => {
 
 exports.createProduct = async (req, res) => {
   try {
-    // Extract image data from the request
     const { images, images02, images03 } = req;
-
-    // Prepare the new product data
     const newProductData = {
       ...req.body,
       image: images,
       image02: images02,
       image03: images03,
     };
-
-    // Create the new product
     const newProduct = await Product.create(newProductData);
-
-    // Respond with the newly created product
     return res.json({ data: newProduct });
   } catch (err) {
-    // Handle errors
     return res.status(500).json({ error: err.message });
   }
 };
-
-// exports.updateProduct = async (req, res) => {
-//   try {
-//     const updateProduct = await Product.findById(req.params.productId);
-//     if (!updateProduct) {
-//       return res.status(404).json({ message: "Product not found" });
-//     }
-//     req.body.image = req.images;
-//     req.body.image02 = req.images02;
-//     req.body.image03 = req.images03;
-//     if (image.length !== 0) {
-//       updateData.image = image;
-//     }
-//     if (image02.length !== 0) {
-//       updateData.image02 = image02;
-//     }
-//     if (image03.length !== 0) {
-//       updateData.image03 = image03;
-//     }
-//     Object.assign(updateProduct, req.body)
-//     await updateProduct.save();
-//     return res.json({ data: updateProduct });
-//   } catch (err) {
-//     return res.status(500).json({ error: err.message });
-//   }
-// };
 
 exports.updateProduct = async (req, res) => {
   try {
